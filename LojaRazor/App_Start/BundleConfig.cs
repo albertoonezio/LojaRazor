08834261fff5c1d8aad9f6f2f9d5df3ae7b91d8c
Alberto <albertomaronezio@hotmai.com>
2019-06-19 18:04:43 -0300

    Termino do Curso de Razor

---
+++ LojaRazor/App_Start/BundleConfig.cs
@@ -0,0 +1,23 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace LojaRazor
+{
+    public class BundleConfig
+    {
+        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            StyleBundle estilos = new StyleBundle("~/bundles/estilos");
+            estilos.Include("~/Content/bootstrap/css/bootstrap.css");
+            estilos.Include("~/Content/Style.css");
+            bundles.Add(estilos);
+
+            StyleBundle scripts = new StyleBundle("~/bundles/scripts");
+            scripts.Include("~/scripts/jquery-1.7.1.js");
+            scripts.Include("~/scripts/jquery.validate.js");
+            scripts.Include("~/scripts/jquery.validate.unobtrusive.js");
+            bundles.Add(scripts);
+        }
+    }
+}
